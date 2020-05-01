43de7285c0d44662fb4727ddff2d04829306fb01
uhyo <uhyo@uhy.ooo>
2020-05-01 23:42:24 +0900

    HTTPS proxy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import http from "http";
+import net from "net";
 
 const server = http.createServer((req, res) => {
   // HTTP リクエストをプロキシする処理
@@ -10,6 +11,25 @@ const server = http.createServer((req, res) => {
     proxyRes.pipe(res);
   });
   req.pipe(proxyReq);
+});
+
+server.on("connect", (req, socket, head) => {
+  console.log(`proxying HTTPS request to ${req.headers.host}`);
+  const u = new URL(`https://${req.headers.host}`);
+  const conn = net.createConnection(
+    {
+      host: u.hostname,
+      port: u.port || 443,
+    },
+    () => {
+      socket.write("HTTP/1.1 200 OK\n\n");
+      conn.pipe(socket);
+    }
+  );
+  if (head) {
+    conn.write(head);
+  }
+  socket.pipe(conn);
 });
 
 server.listen(8111, "0.0.0.0");
